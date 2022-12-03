1abead3aa2794c96b1c90beddabf99b51023b3ff
Trim21 <trim21.me@gmail.com>
2022-12-04 07:43:38 +0800

    refactor: add cf ray as request id

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,22 @@
+import { nanoid } from 'nanoid';
+
 import { createServer } from './server';
 import { logger } from './logger';
 
 const server = createServer({
   logger: logger.child({ name: 'fastify' }, { level: 'warn' }),
   disableRequestLogging: process.env.ENABLE_REQUEST_LOGGING !== 'true',
+  genReqId: (req): string => {
+    if (!req.headers.cf_ray) {
+      return nanoid();
+    }
+
+    if (Array.isArray(req.headers.cf_ray)) {
+      return req.headers.cf_ray.join(';');
+    }
+
+    return req.headers.cf_ray;
+  },
 });
 
 const port = 4000;
