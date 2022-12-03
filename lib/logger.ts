d6f48628b7a4784ad2bda24adfafebfc87c6facd
Trim21 <trim21.me@gmail.com>
2022-12-04 05:35:33 +0800

    refactor: use pino ad logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,37 +1,14 @@
-import * as stream from "stream";
+import { pino } from 'pino';
 
-import * as winston from "winston";
-
-export const logger = winston.createLogger({
-  level: "info",
+export const logger = pino({
+  level: 'info',
+  base: { pid: process.pid },
+  timestamp() {
+    return `,"time":"${new Date().toISOString()}"`;
+  },
+  formatters: {
+    level(level) {
+      return { level };
+    },
+  },
 });
-
-const out = new stream.PassThrough();
-
-out.pipe(process.stdout);
-
-if (process.env.NODE_ENV === "production") {
-  logger.add(
-    new winston.transports.Stream({
-      stream: out,
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-    }),
-  );
-} else {
-  logger.add(
-    new winston.transports.Stream({
-      stream: out,
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.colorize({
-          colors: {
-            info: "blue",
-            warn: "yellow",
-            error: "red",
-          },
-        }),
-        winston.format.simple(),
-      ),
-    }),
-  );
-}
