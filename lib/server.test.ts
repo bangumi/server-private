0a69d371a107e57d499b8d9ac7f5d1f461bd5f23
Trim21 <trim21.me@gmail.com>
2022-12-04 08:40:23 +0800

    tests: add more

--- lib/server.test.ts
+++ lib/server.test.ts
@@ -6,62 +6,6 @@ import { createServer } from './server';
 
 const testClient = createMercuriusTestClient(createServer(), { url: '/v0/graphql' });
 
-describe('subject', () => {
-  test('should get', async () => {
-    await expect(
-      testClient.query(
-        gql`
-          query {
-            subject(id: 8) {
-              name_cn
-            }
-          }
-        `,
-      ),
-    ).resolves.toEqual({
-      data: {
-        subject: { name_cn: 'Code Geass 反叛的鲁路修R2' },
-      },
-    });
-  });
-
-  test('should get episodes, limit', async () => {
-    const query = await testClient.query(
-      gql`
-        query {
-          subject(id: 8) {
-            id
-            episodes(limit: 3) {
-              id
-            }
-          }
-        }
-      `,
-    );
-
-    expect(query.data.subject.id).toBe(8);
-    expect(query.data.subject.episodes).toHaveLength(3);
-  });
-
-  test('should get episodes, -offset', async () => {
-    const query = await testClient.query(
-      gql`
-        query {
-          subject(id: 8) {
-            id
-            episodes(offset: -3) {
-              id
-            }
-          }
-        }
-      `,
-    );
-
-    expect(query.data.subject.id).toBe(8);
-    expect(query.data.subject.episodes).toHaveLength(3);
-  });
-});
-
 describe('auth', () => {
   test('should return current user', async () => {
     await expect(
@@ -124,5 +68,118 @@ describe('auth', () => {
       message: "can't find user by access token",
       statusCode: 401,
     });
+  });
+});
+
+describe('subject', () => {
+  test('should get', async () => {
+    await expect(
+      testClient.query(
+        gql`
+          query {
+            subject(id: 8) {
+              name_cn
+            }
+          }
+        `,
+      ),
+    ).resolves.toEqual({
+      data: {
+        subject: { name_cn: 'Code Geass 反叛的鲁路修R2' },
+      },
+    });
+  });
+
+  test('should not found', async () => {
+    await expect(
+      testClient.query(
+        gql`
+          query {
+            subject(id: 4040404) {
+              name_cn
+            }
+          }
+        `,
+      ),
+    ).resolves.toEqual({
+      data: {
+        subject: null,
+      },
+    });
+  });
+
+  test('should return object for authorized request', async () => {
+    await expect(
+      testClient.query(
+        gql`
+          query {
+            subject(id: 16) {
+              id
+            }
+          }
+        `,
+        {
+          headers: { authorization: 'Bearer a_development_access_token' },
+        },
+      ),
+    ).resolves.toEqual({
+      data: {
+        subject: { id: 16 },
+      },
+    });
+  });
+
+  test('should return null for non-authorized request', async () => {
+    await expect(
+      testClient.query(
+        gql`
+          query {
+            subject(id: 16) {
+              name_cn
+            }
+          }
+        `,
+      ),
+    ).resolves.toEqual({
+      data: {
+        subject: null,
+      },
+    });
+  });
+
+  test('should get episodes, limit', async () => {
+    const query = await testClient.query(
+      gql`
+        query {
+          subject(id: 8) {
+            id
+            episodes(limit: 3) {
+              id
+            }
+          }
+        }
+      `,
+    );
+
+    expect(query.data.subject.id).toBe(8);
+    expect(query.data.subject.episodes).toHaveLength(3);
+  });
+
+  test('should get episodes, -offset', async () => {
+    const query = await testClient.query(
+      gql`
+        query {
+          subject(id: 8) {
+            id
+            episodes(offset: -3) {
+              id
+            }
+          }
+        }
+      `,
+    );
+
+    expect(query.data.subject.id).toBe(8);
+    expect(query.data.subject.episodes).toHaveLength(3);
   });
 });
