16bc6e8563f7e29b1285428ebafb0b743b7bf490
Trim21 <trim21.me@gmail.com>
2022-12-06 21:28:23 +0800

    tests: openapi

--- tests/rest/rest.test.ts
+++ tests/rest/rest.test.ts
@@ -25,4 +25,19 @@ describe('rest', () => {
     expect(res.json()).toEqual({ data: { ID: 382951, username: '382951', nickname: '树洞酱' } });
     expect(res.statusCode).toBe(200);
   });
+
+  test('should build openapi spec', async () => {
+    const app = await createServer();
+
+    const res = await app.inject({
+      url: '/v0.5/openapi.json',
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toMatchObject({
+      info: {
+        title: 'hello',
+      },
+    });
+  });
 });
