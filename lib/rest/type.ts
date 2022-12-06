b9052dcafbdb4774db8e7e5f1b3df5fb865a59d4
Trim21 <trim21.me@gmail.com>
2022-12-06 10:38:40 +0800

    refactor: set Application type to http1 server

--- lib/rest/type.ts
+++ lib/rest/type.ts
@@ -1,16 +1,14 @@
+import type { IncomingMessage, ServerResponse } from 'node:http';
+
 import type { FastifyInstance } from 'fastify';
-import type {
-  RawReplyDefaultExpression,
-  RawRequestDefaultExpression,
-  RawServerDefault,
-} from 'fastify/types/utils';
+import type { RawServerDefault } from 'fastify/types/utils';
 import type { FastifyBaseLogger } from 'fastify/types/logger';
 import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 
 export type App = FastifyInstance<
   RawServerDefault,
-  RawRequestDefaultExpression,
-  RawReplyDefaultExpression,
+  IncomingMessage,
+  ServerResponse,
   FastifyBaseLogger,
   TypeBoxTypeProvider
 >;
