ffabe314493a7e832f604c2c3e6029508901f483
Trim21 <trim21.me@gmail.com>
2022-12-08 05:07:18 +0800

    tests: orm

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -6,10 +6,10 @@ describe('should auth', () => {
   test('current auth', async () => {
     const user = await auth.byToken('a_development_access_token');
     expect(user.permission.report).toBe(true);
-    expect(user.user?.groupID).toBe(10);
+    expect(user.user).toMatchSnapshot();
 
     const cached = await auth.byToken('a_development_access_token');
     expect(cached.permission.report).toBe(true);
-    expect(cached.user?.groupID).toBe(10);
+    expect(cached.user).toMatchSnapshot();
   });
 });
