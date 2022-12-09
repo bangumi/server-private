70a36bdb1a804f752432b68c7d368841382bd8da
Trim21 <trim21.me@gmail.com>
2022-12-09 18:46:47 +0800

    feat: add logout

---
+++ lib/auth/session.test.ts
@@ -0,0 +1,47 @@
+import { expect, test, afterEach, beforeEach } from 'vitest';
+
+import prisma from '../prisma';
+import { create, get, revoke } from './session';
+
+beforeEach(async () => {
+  await prisma.chii_os_web_sessions.deleteMany();
+});
+
+afterEach(async () => {
+  await prisma.chii_os_web_sessions.deleteMany();
+});
+
+test('should create and get session', async () => {
+  const token = await create({
+    id: 382951,
+    regTime: Math.trunc(new Date('2010-01-10 10:05:20').getTime() / 1000),
+  });
+
+  const session = await prisma.chii_os_web_sessions.findFirst({ where: { key: token } });
+
+  expect(session).toBeDefined();
+
+  const s = await get(token);
+
+  expect(s?.user).toBeDefined();
+  expect(s?.user).toMatchSnapshot();
+});
+
+test('should revoke session', async () => {
+  const token = 'fake-random-session-token';
+  await prisma.chii_os_web_sessions.create({
+    data: {
+      key: token,
+      value: Buffer.from(''),
+      user_id: 0,
+      created_at: Math.trunc(Date.now() / 1000),
+      expired_at: Math.trunc(Date.now() / 1000) + 60 * 60 * 242 * 30,
+    },
+  });
+
+  await revoke(token);
+
+  const session = await prisma.chii_os_web_sessions.findFirst({ where: { key: token } });
+
+  expect(session).toBeDefined();
+});
