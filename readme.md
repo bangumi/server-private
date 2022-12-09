37fffa6ef5fa2fbffb9b33516b616ac658e4295c
Trim21 <trim21.me@gmail.com>
2022-12-09 14:43:11 +0800

    docs: add tests

--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ mercurius + nexus + prisma
 <https://api.bgm.tv/v0/altair/>
 
 [schema](./lib/graphql/schema.gen.graphql)
+
+## 测试
+
+jest 对于 esm 的支持不好，所以使用 vitest 作为测试框架。
+
+需要 mysql 和 redis。
+
+没有针对 vitest 的 HMR 功能进行兼容，所以不要使用 vitest 的 `watch` 模式或者 `ui` 功能。
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    watch: false,
+    environment: 'node',
     setupFiles: ['./tests/setup.ts'],
     coverage: {
       exclude: ['/node_modules/', 'lib/generated/'],
