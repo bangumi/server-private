b27222f8b2ea5aa0f0c8a8918b2af763959c5e98
Trim21 <trim21.me@gmail.com>
2022-12-12 01:35:19 +0800

    perf: generate random in seprated thread

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -4,32 +4,41 @@ const base62Chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW
 const base62CharsLength = base62Chars.length;
 const base62MaxByte = 255 - (256 % base62Chars.length);
 
-export function randomBase62String(length: number): string {
+export async function randomBase62String(length: number): Promise<string> {
   const step = Math.trunc(length + length / 4);
+  const buf = Buffer.allocUnsafe(step);
+
   let result = '';
 
   let char: string | undefined;
-
   // eslint-disable-next-line no-constant-condition,@typescript-eslint/no-unnecessary-condition
   while (true) {
-    const randomBytes = crypto.randomBytes(step);
-
-    for (const rb of randomBytes) {
+    const b = await randomFill(buf);
+    for (const rb of b) {
       if (rb > base62MaxByte) {
         continue;
       }
 
       char = base62Chars[rb % base62CharsLength];
 
-      if (char === undefined) {
-        continue;
-      }
-
-      result += char;
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      result += char!;
 
       if (result.length === length) {
         return result;
       }
     }
   }
+}
+
+async function randomFill(buf: Buffer): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    crypto.randomFill(buf, (err, buf) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf);
+      }
+    });
+  });
 }
