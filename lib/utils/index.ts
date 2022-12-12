775a2293d444cc3a1ea7e7477bab826dcf6d744c
Trim21 <trim21.me@gmail.com>
2022-12-12 10:10:25 +0800

    refactor: use nanoid

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,44 +1,10 @@
-import * as crypto from 'node:crypto';
+import { customAlphabet } from 'nanoid/async';
 
 const base62Chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const base62CharsLength = base62Chars.length;
-const base62MaxByte = 255 - (256 % base62Chars.length);
-
-export async function randomBase62String(length: number): Promise<string> {
-  const step = Math.trunc(length + length / 4);
-  const buf = Buffer.allocUnsafe(step);
-
-  let result = '';
-
-  let char: string | undefined;
-  // eslint-disable-next-line no-constant-condition,@typescript-eslint/no-unnecessary-condition
-  while (true) {
-    const b = await randomFill(buf);
-    for (const rb of b) {
-      if (rb > base62MaxByte) {
-        continue;
-      }
-
-      char = base62Chars[rb % base62CharsLength];
-
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      result += char!;
-
-      if (result.length === length) {
-        return result;
-      }
-    }
-  }
+if (base62Chars.length !== 62) {
+  throw new TypeError('characters is not 62 length');
 }
 
-async function randomFill(buf: Buffer): Promise<Buffer> {
-  return new Promise((resolve, reject) => {
-    crypto.randomFill(buf, (err, buf) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buf);
-      }
-    });
-  });
-}
+const generator = customAlphabet(base62Chars);
+
+export const randomBase62String = (size: number) => generator(size);
