22cb59b0dd9a87755ae749c0b42f15f25c2c44ad
Trim21 <trim21.me@gmail.com>
2022-12-15 17:29:06 +0800

    ci: test docker image

--- lib/main.ts
+++ lib/main.ts
@@ -4,6 +4,13 @@ import { production } from './config';
 import { logger } from './logger';
 import { createServer } from './server';
 
+if (process.argv.includes('--help') || process.argv.includes('-h')) {
+  // eslint-disable-next-line no-console
+  console.log('check ./lib/config.ts for all available env');
+  // eslint-disable-next-line n/no-process-exit,unicorn/no-process-exit
+  process.exit(0);
+}
+
 const server = await createServer({
   logger: logger.child({ name: 'fastify' }, { level: production ? 'warn' : 'info' }),
   disableRequestLogging: process.env.ENABLE_REQUEST_LOGGING !== 'true',
