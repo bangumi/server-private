5dbe6acc8e9e6b18d7c66bdfa645d0d0bd4ebdb9
Trim21 <trim21.me@gmail.com>
2022-12-17 19:59:49 +0800

    chore: add version to logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,22 +2,16 @@ import process from 'node:process';
 
 import { pino } from 'pino';
 
+import { pkg, production } from './config';
+
 export const logger = pino({
   level: 'info',
-  base: { pid: process.pid },
+  base: production ? { pid: process.pid, version: pkg.version } : undefined,
   timestamp() {
     return `,"time":"${new Date().toISOString()}"`;
   },
   /** 使用 config.ts 的 production 变量会导致循环 import，所以直接从环境变量中读取 */
-  transport:
-    process.env.NODE_ENV === 'production'
-      ? undefined
-      : {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-          },
-        },
+  transport: production ? undefined : { target: 'pino-pretty', options: { colorize: true } },
   formatters: {
     level(level) {
       return { level };
