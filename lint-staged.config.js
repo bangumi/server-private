b421919d0ca21a2c2c7acbf7c4a2ae3b7708dae7
Trim21 <trim21.me@gmail.com>
2022-12-18 22:04:47 +0800

    build: use a shared base docker imge

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@
 export default {
   '*.{md,html,json,cjs,mjs,js,yml,yaml}': 'prettier -w',
   '**/pre-commit': 'prettier -w',
-  Dockerfile: 'prettier -w',
+  '*.dockerfile': 'prettier -w',
   '*.ts': ['eslint --fix', 'prettier -w', () => 'tsc -p tsconfig.json --pretty --noEmit'],
 };
