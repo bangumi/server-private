16b32957c92fea1f9b52111715170ca8277dc44d
Trim21 <trim21.me@gmail.com>
2022-12-18 23:51:27 +0800

    chore: add liquid to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 // lint-staged.config.js
 export default {
-  '*.{md,html,json,cjs,mjs,js,yml,yaml}': 'prettier -w',
+  '*.{md,html,json,cjs,mjs,js,yml,yaml,liquid}': 'prettier -w',
   '**/pre-commit': 'prettier -w',
   '*.dockerfile': 'prettier -w',
   '*.ts': ['eslint --fix', 'prettier -w', () => 'tsc -p tsconfig.json --pretty --noEmit'],
