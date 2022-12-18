6f2c34832faab64f9d42db831edbb2e07b4634fc
Trim21 <trim21.me@gmail.com>
2022-12-18 18:33:01 +0800

    chore: lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 // lint-staged.config.js
 export default {
-  '*.{ts,md,html,json,cjs,mjs,js,yml,yaml,prisma}': 'prettier -w',
+  '*.{md,html,json,cjs,mjs,js,yml,yaml,prisma}': 'prettier -w',
   '**/pre-commit': 'prettier -w',
   Dockerfile: 'prettier -w',
-  '*.ts': ['eslint', () => 'tsc -p tsconfig.json --pretty --noEmit'],
+  '*.ts': ['eslint --fix', 'prettier -w', () => 'tsc -p tsconfig.json --pretty --noEmit'],
 };
