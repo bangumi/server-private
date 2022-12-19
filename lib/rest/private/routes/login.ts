2f7c2f199cc78a10c3986ef1ea5378e743e4557a
Trim21 <trim21.me@gmail.com>
2022-12-19 23:41:05 +0800

    refactor: remove hcaptcha (#21)

--- lib/rest/private/routes/login.ts
+++ lib/rest/private/routes/login.ts
@@ -4,8 +4,7 @@ import httpCodes from 'http-status-codes';
 
 import { comparePassword, NeedLoginError } from '../../../auth';
 import * as session from '../../../auth/session';
-import { HCAPTCHA_SECRET_KEY, redisPrefix, TURNSTILE_SECRET_KEY } from '../../../config';
-import { createHCaptchaDriver } from '../../../externals/hcaptcha';
+import { redisPrefix, TURNSTILE_SECRET_KEY } from '../../../config';
 import { createTurnstileDriver } from '../../../externals/turnstile';
 import { Tag } from '../../../openapi';
 import redis from '../../../redis';
@@ -42,8 +41,6 @@ export async function setup(app: App) {
     duration: 600,
   });
 
-  const hCaptcha = createHCaptchaDriver(HCAPTCHA_SECRET_KEY);
-
   app.addSchema(res.User);
   app.addSchema(res.Error);
   app.addSchema(res.ValidationError);
@@ -77,100 +74,6 @@ export async function setup(app: App) {
 
       void res.clearCookie(CookieKey);
       await session.revoke(req.cookies.sessionID);
-    },
-  );
-
-  app.post(
-    '/login',
-    {
-      schema: {
-        description: `需要 [hCaptcha的验证码](https://docs.hcaptcha.com/#add-the-hcaptcha-widget-to-your-webpage)
-
-site-key 是 \`4874acee-9c6e-4e47-99ad-e2ea1606961f\``,
-        operationId: 'login',
-        tags: [Tag.Auth],
-        response: {
-          200: t.Ref(res.User, {
-            headers: {
-              'Set-Cookie': t.String({ description: 'example: "sessionID=12345abc"' }),
-            },
-          }),
-          400: t.Ref(res.ValidationError),
-          401: t.Ref(res.Error, {
-            description: '验证码错误/账号密码不匹配',
-            headers: {
-              'X-RateLimit-Remaining': t.Integer({ description: 'remaining rate limit' }),
-              'X-RateLimit-Limit': t.Integer({ description: 'total limit per 10 minutes' }),
-              'X-RateLimit-Reset': t.Integer({ description: 'seconds to reset rate limit' }),
-            },
-            'x-examples': res.formatErrors(new CaptchaError(), new EmailOrPasswordError()),
-          }),
-          429: t.Ref(res.Error, {
-            description: '失败次数太多，需要过一段时间再重试',
-            headers: {
-              'X-RateLimit-Remaining': t.Integer({ description: 'remaining rate limit' }),
-              'X-RateLimit-Limit': t.Integer({ description: 'limit per 10 minutes' }),
-              'X-RateLimit-Reset': t.Integer({ description: 'seconds to reset rate limit' }),
-            },
-            examples: [res.formatError(TooManyRequestsError())],
-          }),
-        },
-        body: t.Object(
-          {
-            email: t.String({ minLength: 1 }),
-            password: t.String({ minLength: 1 }),
-            'h-captcha-response': t.String({ minLength: 1 }),
-          },
-          {
-            examples: [
-              {
-                email: 'treeholechan@gmail.com',
-                password: 'lovemeplease',
-                'h-captcha-response': '10000000-aaaa-bbbb-cccc-000000000001',
-              },
-            ],
-          },
-        ),
-      },
-    },
-    async function handler(
-      { body: { email, password, 'h-captcha-response': hCaptchaResponse }, clientIP },
-      reply,
-    ): Promise<res.IUser> {
-      const { remain, reset } = await limiter.get(`${redisPrefix}-login-rate-limit-${clientIP}`);
-      void reply.headers({
-        'X-RateLimit-Remaining': remain,
-        'X-RateLimit-Limit': LimitInTimeWindow,
-        'X-RateLimit-Reset': reset,
-      });
-      if (remain <= 0) {
-        throw new TooManyRequestsError();
-      }
-
-      if (!(await hCaptcha.verify(hCaptchaResponse))) {
-        throw new CaptchaError();
-      }
-
-      const user = await UserRepo.findOne({ where: { email } });
-      if (!user) {
-        throw new EmailOrPasswordError();
-      }
-      if (!(await comparePassword(user.passwordCrypt, password))) {
-        throw new EmailOrPasswordError();
-      }
-
-      const token = await session.create({
-        id: user.id,
-        regTime: user.regdate,
-      });
-
-      void reply.cookie(CookieKey, token, { sameSite: 'strict' });
-
-      return {
-        ...user,
-        user_group: user.groupid,
-        avatar: avatar(user.avatar),
-      };
     },
   );
 
