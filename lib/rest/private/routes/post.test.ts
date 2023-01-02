c9042bab5ffa446c39d57ca8ac919014181558b3
Trim21 <trim21.me@gmail.com>
2023-01-02 11:31:51 +0800

    feat: new api to edit group reply (#32)

---
+++ lib/rest/private/routes/post.test.ts
@@ -0,0 +1,64 @@
+import { test, expect, beforeEach } from 'vitest';
+
+import { emptyAuth } from '@app/lib/auth';
+import * as orm from '@app/lib/orm';
+import { createTestServer } from '@app/tests/utils';
+
+import { setup } from './post';
+
+beforeEach(async () => {
+  await orm.GroupPostRepo.update(
+    {
+      id: 2177419,
+    },
+    {
+      content: 'before-test',
+    },
+  );
+});
+
+test('should edit post', async () => {
+  const app = createTestServer({
+    auth: {
+      ...emptyAuth(),
+      login: true,
+      userID: 287622,
+    },
+  });
+
+  await app.register(setup);
+
+  const res = await app.inject({
+    url: '/groups/-/posts/2177419',
+    method: 'put',
+    payload: { text: 'new content' },
+  });
+
+  expect(res.statusCode).toBe(200);
+
+  const pst = await orm.GroupPostRepo.findOneBy({
+    id: 2177419,
+  });
+
+  expect(pst?.content).toBe('new content');
+});
+
+test('should not edit post', async () => {
+  const app = createTestServer({
+    auth: {
+      ...emptyAuth(),
+      login: true,
+      userID: 287622 + 1,
+    },
+  });
+
+  await app.register(setup);
+
+  const res = await app.inject({
+    url: '/groups/-/posts/2177419',
+    method: 'put',
+    payload: { text: 'new content' },
+  });
+
+  expect(res.statusCode).toBe(401);
+});
