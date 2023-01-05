d8bceccda0fde6f2cc9212b3c47c03fa8f9869cc
Trim21 <trim21.me@gmail.com>
2023-01-05 08:16:38 +0800

    refactor: add default token size

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -5,6 +5,6 @@ if (base62Chars.length !== 62) {
   throw new TypeError('characters is not 62 length');
 }
 
-const generator = customAlphabet(base62Chars);
+const generator = customAlphabet(base62Chars, 32);
 
 export const randomBase62String = (size: number) => generator(size);
