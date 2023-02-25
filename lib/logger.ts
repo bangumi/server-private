64544ee80ee1025db43601903bd624cd1646f4da
Trim21 <trim21.me@gmail.com>
2023-02-25 15:36:30 +0800

    chore: json logging in stage env

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,18 +2,24 @@ import * as process from 'node:process';
 
 import { pino } from 'pino';
 
-import { production, testing, VERSION } from './config';
+import { production, stage, testing, VERSION } from './config';
 
 export const logger = testing
   ? pino({ level: 'error' })
   : pino({
       level: 'info',
-      base: production ? { pid: process.pid, version: VERSION } : undefined,
+      base: production || stage ? { pid: process.pid, version: VERSION } : undefined,
       timestamp() {
         return `,"time":"${new Date().toISOString()}"`;
       },
       /** 使用 config.ts 的 production 变量会导致循环 import，所以直接从环境变量中读取 */
-      transport: production ? undefined : { target: 'pino-pretty', options: { colorize: true } },
+      transport:
+        production || stage
+          ? undefined
+          : {
+              target: 'pino-pretty',
+              options: { colorize: true },
+            },
       formatters: {
         level(level) {
           return { level };
