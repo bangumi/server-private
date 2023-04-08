3c83fb1c44a442ead37b17d51676e59d52a0ad40
Trim21 <trim21.me@gmail.com>
2023-04-09 05:07:47 +0800

    feat: topic reactions (#210)

---
+++ lib/like.ts
@@ -0,0 +1,32 @@
+import * as lo from 'lodash-es';
+
+import { LikeRepo } from '@app/lib/orm';
+
+export interface Reaction {
+  selected: boolean;
+  total: number;
+  value: number;
+}
+
+export async function fetchGroupTopic(
+  id: number,
+  uid: number,
+): Promise<Record<number, Reaction[]>> {
+  const data = await LikeRepo.findBy({ mainID: id });
+
+  const r = lo.groupBy(data, (x) => x.relatedID);
+
+  return lo.mapValues(r, (v): Reaction[] => {
+    return Object.entries(
+      lo.groupBy(v, (a) => {
+        return a.value;
+      }),
+    ).map(([key, values]) => {
+      return {
+        selected: values.some((x) => x.uid === uid),
+        total: values.length,
+        value: Number(key),
+      };
+    });
+  });
+}
