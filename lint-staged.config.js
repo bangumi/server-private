2ae57906bc5f0117c48c62090a1af216f6ae6bc9
Trim21 <trim21.me@gmail.com>
2023-04-22 16:47:59 +0800

    chore: use pnpm in pre-commit hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,11 @@
 // lint-staged.config.js
 export default {
   '*.{md,html,json,cts,cjs,mjs,js,yml,yaml,liquid,dockerfile}': 'prettier -w',
-  '*.{cts,cjs,mjs,js}': 'eslint --fix',
-  '**/pre-commit': 'prettier -w',
+  '*.{cts,cjs,mjs,js}': 'eslint --quiet --fix',
   '.{git,docker}ignore': 'prettier -w',
   '*.ts': [
     'prettier -w',
     () => 'tsc -p tsconfig.json --pretty --noEmit', // do not pass filenames
-    'eslint --fix',
+    'eslint --quiet --fix',
   ],
 };
