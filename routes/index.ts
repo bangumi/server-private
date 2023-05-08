fcae11c423e3e2cc2b025cf4efeb8cc5016d6188
Trim21 <trim21.me@gmail.com>
2023-05-09 07:43:07 +0800

    refactor: import image static files

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,9 @@
+import * as path from 'node:path';
+
+import { fastifyStatic } from '@fastify/static';
+
 import type { IAuth } from '@app/lib/auth';
+import { projectRoot } from '@app/lib/config.ts';
 import { logger } from '@app/lib/logger.ts';
 import * as demo from '@app/routes/demo';
 import * as privateAPI from '@app/routes/private';
@@ -14,7 +19,11 @@ export async function setup(app: App) {
   await app.register(publicAPI.setup, { prefix: '/v0.5' });
   await app.register(demo.setup, { prefix: '/demo/' });
 
-  app.get('/img/*', (req, res) => res.redirect('https://bgm.tv' + req.url));
+  await app.register(fastifyStatic, {
+    root: path.resolve(projectRoot, 'static/img/'),
+    dotfiles: 'ignore',
+    prefix: '/img/',
+  });
 }
 
 declare module 'fastify' {
