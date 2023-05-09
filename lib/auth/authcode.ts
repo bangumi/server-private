dfa7074a9b1848161ee6f07ee7d944d5374fb697
Trim21 <trim21.me@gmail.com>
2023-05-10 02:52:08 +0800

    refactor: add file comment

--- lib/auth/authcode.ts
+++ lib/auth/authcode.ts
@@ -1,32 +1,31 @@
+/** Discuz encryption/decryption, don't use it for non-legacy encryption or decryption */
+
 import { md5 } from '@app/lib/utils';
 
 /* eslint-disable unicorn/prefer-code-point,@typescript-eslint/no-non-null-assertion */
 
 /**
  * @param str - 加密内容
- * @param operation - 加密动作
+ * @param decode - 解密/加密
  * @param key - 密钥
  * @returns 加密串
  */
-function authcode(str: string, operation: 'DECODE' | 'ENCODE', key = '') {
+function authCode(str: string, decode: boolean, key = '') {
   key = md5(key);
 
-  const buf =
-    operation === 'DECODE'
-      ? Uint8Array.from(atob(str), (c) => c.charCodeAt(0))
-      : Buffer.from(md5(str + key) + str);
+  const buf = decode ? Buffer.from(str, 'base64') : Buffer.from(md5(str + key) + str);
 
   let result = '';
 
   const box: number[] = Array.from({ length: 256 });
-  const rndkey: number[] = [];
+  const randomKey: number[] = Array.from({ length: 256 });
   for (let i = 0; i < 256; i++) {
-    rndkey[i] = key.charCodeAt(i % key.length);
+    randomKey[i] = key.charCodeAt(i % key.length);
     box[i] = i;
   }
 
   for (let i = 0, j = 0; i < 256; i++) {
-    j = (j + box[i]! + rndkey[i]!) % 256;
+    j = (j + box[i]! + randomKey[i]!) % 256;
 
     const tmp = box[i]!;
     box[i] = box[j]!;
@@ -41,29 +40,24 @@ function authcode(str: string, operation: 'DECODE' | 'ENCODE', key = '') {
     box[a] = box[j]!;
     box[j] = tmp;
 
-    result += chr(buf[i]! ^ box[(box[a]! + box[j]!) % 256]!);
+    result += String.fromCodePoint(buf[i]! ^ box[(box[a]! + box[j]!) % 256]!);
   }
 
-  if (operation == 'DECODE') {
+  if (decode) {
     // eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with
     if (result.slice(0, 8) === md5(result.slice(8) + key).slice(0, 8)) {
       return result.slice(8);
     }
 
     return '';
-  } else {
-    return Buffer.from(result).toString('base64').replaceAll('=', '');
   }
-}
-
-function chr(s: number) {
-  return String.fromCharCode(s);
+  return Buffer.from(result).toString('base64').replaceAll('=', '');
 }
 
 export function decode(str: string, key: string) {
-  return authcode(str, 'DECODE', key);
+  return authCode(str, true, key);
 }
 
 export function encode(str: string, key = '') {
-  return authcode(str, 'ENCODE', key);
+  return authCode(str, false, key);
 }
