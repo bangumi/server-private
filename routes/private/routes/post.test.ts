17582ff3aa1808afa0224cc302e71b4caaa8d5fa
MisakaTAT <i@mikuac.com>
2024-06-26 11:43:38 +0800

    chore: fix post test

--- routes/private/routes/post.test.ts
+++ routes/private/routes/post.test.ts
@@ -186,7 +186,7 @@ describe('create group post reply', () => {
     vi.spyOn(Topic, 'createTopicReply').mockImplementation(createTopicReply);
     vi.spyOn(Notify, 'create').mockImplementation(notifyMock);
     vi.spyOn(Topic, 'fetchDetail').mockImplementationOnce(
-      (_: IAuth, type: 'group', id: number): Promise<ITopicDetails | null> => {
+      (_: IAuth, type: 'group' | 'subject', id: number): Promise<ITopicDetails | null> => {
         if (id !== 371602) {
           return Promise.resolve(null);
         }
