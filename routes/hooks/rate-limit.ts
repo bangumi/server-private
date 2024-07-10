82b165babf7956b11474a9b5c7cb6219a384954d
MisakaTAT <i@mikuac.com>
2024-07-10 17:58:04 +0800

    chore: replace rate limiter cache with redis

--- routes/hooks/rate-limit.ts
+++ routes/hooks/rate-limit.ts
@@ -2,60 +2,7 @@
 import { createError } from '@fastify/error';
 
 import type { IAuth } from '@app/lib/auth';
-import NodeCache from '@app/vendor/node-cache';
-
-const cache = new NodeCache();
-
-interface LimitRule {
-  limit: string;
-  validate?: number;
-  hibernate?: number;
-}
-
-const LIMIT_RULES: Record<string, LimitRule> = {
-  app: { limit: '5/10' },
-  ep: { limit: '10/10' },
-  blog: { limit: '3/30' },
-  index: { limit: '3/30' },
-  group: { limit: '3/30' },
-  doujin: { limit: '3/30' },
-  event: { limit: '1/60' },
-  event_topics: { limit: '3/30' },
-  subject: { limit: '3/30' },
-  club_topics: { limit: '1/30' },
-  crt_post: { limit: '1/1', validate: 7, hibernate: 5 },
-  prsn_post: { limit: '1/1', validate: 7, hibernate: 5 },
-  like: { limit: '2/1' },
-};
-
-const parseLimit = (limit: string) => {
-  const [count, minutes] = limit.split('/').map(Number) as [number, number];
-  return { count, minutes };
-};
-
-const isRateLimited = (key: number, type: string) => {
-  const limitRules = LIMIT_RULES[type];
-  if (!limitRules) {
-    throw new Error('Invalid rate limit type');
-  }
-
-  const { count, minutes } = parseLimit(limitRules.limit);
-
-  const now = Date.now();
-  const cacheKey = `${key}:${type}`;
-  const userRequests = cache.get<number[]>(cacheKey) || [];
-
-  // Remove expired timestamps
-  const validRequests = userRequests.filter((timestamp) => now - timestamp < minutes * 60 * 1000);
-
-  if (validRequests.length >= count) {
-    return true;
-  }
-
-  validRequests.push(now);
-  cache.set(cacheKey, validRequests, minutes * 60);
-  return false;
-};
+import { TypedCache } from '@app/lib/cache';
 
 export const InvalidRateLimitType = createError<[string]>(
   'RATE_LIMIT_TYPE_INVALID',
@@ -69,12 +16,58 @@ export const RateLimitExceeded = createError<[]>(
   429,
 );
 
-export const rateLimiter = (type: string) => async (req: { auth: IAuth }) => {
-  if (!LIMIT_RULES[type]) {
+const cache = TypedCache<string, number[]>((key) => key);
+
+type LimitType = keyof typeof LIMIT_RULES;
+
+interface LimitRule {
+  count: number;
+  minutes: number;
+  validate?: number;
+  hibernate?: number;
+}
+
+const LIMIT_RULES: Record<string, LimitRule> = {
+  app: { count: 5, minutes: 10 },
+  ep: { count: 10, minutes: 10 },
+  blog: { count: 3, minutes: 30 },
+  index: { count: 3, minutes: 30 },
+  group: { count: 3, minutes: 30 },
+  doujin: { count: 3, minutes: 30 },
+  event: { count: 1, minutes: 60 },
+  event_topics: { count: 3, minutes: 30 },
+  subject: { count: 3, minutes: 30 },
+  club_topics: { count: 1, minutes: 30 },
+  crt_post: { count: 1, minutes: 1, validate: 7, hibernate: 5 },
+  prsn_post: { count: 1, minutes: 1, validate: 7, hibernate: 5 },
+  like: { count: 2, minutes: 1 },
+};
+
+const isRateLimited = async (uid: number, type: LimitType) => {
+  const limitRule = LIMIT_RULES[type];
+  if (!limitRule) {
     throw new InvalidRateLimitType(type);
   }
+  const { count, minutes } = limitRule;
 
-  if (isRateLimited(req.auth.userID, type)) {
+  const now = Date.now();
+  const cacheKey = `rate-limit:${type}:${uid}`;
+
+  const userRequests = (await cache.get(cacheKey)) || [];
+
+  const validRequests = userRequests.filter((timestamp) => now - timestamp < minutes * 60 * 1000);
+
+  if (validRequests.length >= count) {
+    return true;
+  }
+
+  validRequests.push(now);
+  await cache.set(cacheKey, validRequests, minutes * 60);
+  return false;
+};
+
+export const rateLimiter = (type: LimitType) => async (req: { auth: IAuth }) => {
+  if (await isRateLimited(req.auth.userID, type)) {
     throw new RateLimitExceeded();
   }
 };
