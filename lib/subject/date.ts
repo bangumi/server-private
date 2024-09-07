4f78b69f9a9af0b5d37992dff2fe6c72973dee4a
Trim21 <trim21.me@gmail.com>
2024-09-08 00:57:01 +0800

    fix parsing date

--- lib/subject/date.ts
+++ lib/subject/date.ts
@@ -65,8 +65,7 @@ const simple_patterns = [
   /（(?<year>\d{4})-(?<month>\d{1,2})-(?<day>\d{1,2})）$/, //（YYYY-MM-DD）
   /^(?<year>\d{4})-(?<month>\d{1,2})-(?<day>\d{1,2})$/, // YYYY-MM-DD"
   /^(?<year>\d{4})-(?<month>\d{1,2})-(?<day>\d{1,2})[ ([（].*$/, // YYYY-MM-DD...
-  /^(?<year>\d{4})年(?<month>\d{1,2})月(?<day>\d{1,2})日/, // YYYY年MM月DD日
-  /^(?<year>\d{4})年(?:(?<month>\d{1,2})月)?(?:(?<day>\d{1,2})日)?/, // YYYY年
+  /^(?<year>\d{4})年(?:(?<month>\d{1,2})月)?(?:(?<day>\d{1,2})日)?/, // YYYY年(MM月)?(DD日)?
 ];
 
 const simple_dash_patterns = [
