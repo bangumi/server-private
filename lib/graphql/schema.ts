3d0a0cba687e98925473c7a74112092b30ed8bc0
Trim21 <trim21.me@gmail.com>
2024-09-28 12:58:03 +0800

    feat: make it run as federation

--- lib/graphql/schema.ts
+++ lib/graphql/schema.ts
@@ -1,14 +1,38 @@
+import * as fsp from 'node:fs/promises';
 import * as path from 'node:path';
 
-import { makeSchema } from 'nexus';
+import { extendType, makeSchema, objectType } from 'nexus';
 
 import { projectRoot } from '@app/lib/config.ts';
 
 import types from './types/index.ts';
 
+const _Service = objectType({
+  name: '_Service',
+  definition(t) {
+    t.nonNull.string('sdl');
+  },
+});
+
+const federationSupport = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.field('_service', {
+      type: '_Service',
+      async resolve() {
+        return {
+          sdl: await fsp.readFile(
+            path.join(projectRoot, 'lib', 'graphql', 'schema.gen.graphql'),
+            'utf8',
+          ),
+        };
+      },
+    });
+  },
+});
+
 export const schema = makeSchema({
-  types,
-  plugins: [],
+  types: [types, _Service, federationSupport],
   outputs: {
     schema: path.join(projectRoot, 'lib', 'graphql', 'schema.gen.graphql'),
   },
