e3cd16bf7c3d88a4bb873a3b85036104eb358633
everpcpc <git@everpcpc.com>
2024-10-06 13:57:57 +0800

    fix: get subject comments order by lastTouch desc

--- routes/private/routes/post.ts
+++ routes/private/routes/post.ts
@@ -865,6 +865,7 @@ dev.bgm38.com 域名使用测试用的 site-key \`1x00000000000000000000AA\``,
       const count = await orm.SubjectInterestRepo.count({ where });
       const comments = await orm.SubjectInterestRepo.find({
         where: where,
+        order: { lastTouch: 'desc' },
         skip: query.offset,
         take: query.limit,
       });
