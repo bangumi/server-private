9654067af0909b407fdb8cfd0338e90e10ba9955
everpcpc <git@everpcpc.com>
2024-10-09 13:16:19 +0800

    z

--- drizzle/orm.ts
+++ drizzle/orm.ts
@@ -3,3 +3,9 @@ import type * as schema from './schema.ts';
 export type ISubject = typeof schema.chiiSubjects.$inferSelect;
 export type ISubjectFields = typeof schema.chiiSubjectFields.$inferSelect;
 export type ISubjectInterests = typeof schema.chiiSubjectInterests.$inferSelect;
+
+export type ICharacter = typeof schema.chiiCharacters.$inferSelect;
+
+export type IPerson = typeof schema.chiiPersons.$inferSelect;
+
+export type IPersonCollect = typeof schema.chiiPersonCollects.$inferSelect;
