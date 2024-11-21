3683154a2c7a96a8765607b187e33844aafa662f
everpcpc <git@everpcpc.com>
2024-11-22 00:03:50 +0800

    z

--- build.mjs
+++ build.mjs
@@ -6,14 +6,12 @@ import * as url from 'node:url';
 import * as esbuild from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
-await esbuild.build({
-  entryPoints: ['bin/main.ts'],
+const buildConfigs = {
   bundle: true,
   platform: 'node',
   target: 'node20',
   format: 'esm',
   sourcemap: 'linked',
-  outfile: 'dist/index.mjs',
   plugins: [
     nodeExternalsPlugin(),
     {
@@ -30,30 +28,16 @@ await esbuild.build({
       },
     },
   ],
+};
+
+await esbuild.build({
+  entryPoints: ['bin/main.ts'],
+  outfile: 'dist/index.mjs',
+  ...buildConfigs,
 });
 
 await esbuild.build({
   entryPoints: ['bin/cron.ts'],
-  bundle: true,
-  platform: 'node',
-  target: 'node20',
-  format: 'esm',
-  sourcemap: 'linked',
-  outfile: 'dist/cron.mjs',
-  plugins: [
-    nodeExternalsPlugin(),
-    {
-      name: 'import.meta.url',
-      // eslint-disable-next-line @typescript-eslint/unbound-method
-      setup({ onLoad }) {
-        onLoad({ filter: /.*\.ts/g, namespace: 'file' }, (args) => {
-          let code = fs.readFileSync(args.path, 'utf8');
-          code = code
-            .replaceAll(/\bimport\.meta\.url\b/g, JSON.stringify(url.pathToFileURL(args.path)))
-            .replaceAll(/\bimport\.meta\.dirname\b/g, JSON.stringify(path.dirname(args.path)));
-          return { contents: code, loader: 'ts' };
-        });
-      },
-    },
-  ],
+  outfile: 'dist/cron.js',
+  ...buildConfigs,
 });
