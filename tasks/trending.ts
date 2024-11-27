360b8afd77d696f7edb5af4c70e4ce674f659000
everpcpc <everpcpc@users.noreply.github.com>
2024-11-27 08:37:40 +0800

    chore: add more logs for cron (#832)

--- tasks/trending.ts
+++ tasks/trending.ts
@@ -1,9 +1,12 @@
+import { logger } from '@app/lib/logger';
 import { SubjectTypeValues } from '@app/lib/subject/type.ts';
 import { updateTrendingSubjects } from '@app/lib/trending/subject.ts';
 import { TrendingPeriod } from '@app/lib/trending/type.ts';
 
 export async function trendingSubjects() {
+  logger.info('Updating trending subjects...');
   for (const subjectType of SubjectTypeValues) {
+    logger.info(`Updating trending subjects for ${subjectType}...`);
     await updateTrendingSubjects(subjectType, TrendingPeriod.Month);
   }
 }
