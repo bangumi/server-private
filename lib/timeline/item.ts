ef460021837a3a50bed00b9a7857b8d7b6637d18
everpcpc <git@everpcpc.com>
2024-12-20 11:19:41 +0800

    z

--- lib/timeline/item.ts
+++ lib/timeline/item.ts
@@ -1,3 +1,5 @@
+import * as php from '@trim21/php-serialize';
+
 import { db, op } from '@app/drizzle/db.ts';
 import type * as orm from '@app/drizzle/orm.ts';
 import * as schema from '@app/drizzle/schema';
@@ -5,7 +7,206 @@ import redis from '@app/lib/redis.ts';
 import type * as res from '@app/lib/types/res.ts';
 
 import { getItemCacheKey } from './cache.ts';
-import { parse as parseTimelineMemo } from './memo';
+import type * as memo from './memo';
+import { TimelineCat } from './type';
+
+export function parseTimelineMemo(cat: TimelineCat, type: number, batch: boolean, data: string) {
+  if (data === '') {
+    return {};
+  }
+  switch (cat) {
+    case TimelineCat.Daily: {
+      switch (type) {
+        case 2: {
+          const users = [];
+          if (batch) {
+            const info = php.parse(data) as memo.UserBatch;
+            for (const [_, value] of Object.entries(info)) {
+              users.push(value);
+            }
+          } else {
+            const info = php.parse(data) as memo.User;
+            users.push(info);
+          }
+          return {
+            daily: {
+              user: users.map((u) => ({
+                uid: Number(u.uid),
+                username: u.username,
+                nickname: u.nickname,
+              })),
+            },
+          };
+        }
+        case 3:
+        case 4: {
+          const groups = [];
+          if (batch) {
+            const info = php.parse(data) as memo.GroupBatch;
+            for (const [_, value] of Object.entries(info)) {
+              groups.push(value);
+            }
+          } else {
+            const info = php.parse(data) as memo.Group;
+            groups.push(info);
+          }
+          return {
+            daily: {
+              group: groups.map((g) => ({
+                id: Number(g.grp_id),
+                name: g.grp_name,
+                title: g.grp_title,
+                desc: g.grp_desc,
+              })),
+            },
+          };
+        }
+        default: {
+          return {};
+        }
+      }
+    }
+    case TimelineCat.Wiki: {
+      const info = php.parse(data) as memo.NewSubject;
+      return {
+        wiki: {
+          subject: {
+            id: info.subject_id,
+            name: info.subject_name,
+            nameCN: info.subject_name_cn,
+          },
+        },
+      };
+    }
+    case TimelineCat.Subject: {
+      const subjects = [];
+      if (batch) {
+        const info = php.parse(data) as memo.SubjectBatch;
+        for (const [_, value] of Object.entries(info)) {
+          subjects.push(value);
+        }
+      } else {
+        const info = php.parse(data) as memo.Subject;
+        subjects.push(info);
+      }
+      return {
+        subject: subjects.map((s) => ({
+          id: Number(s.subject_id),
+          type: Number(s.subject_type_id),
+          name: s.subject_name,
+          nameCN: s.subject_name_cn,
+          series: s.subject_series === '1',
+          comment: s.collect_comment,
+          rate: Number(s.collect_rate),
+        })),
+      };
+    }
+    case TimelineCat.Progress: {
+      if (type === 0) {
+        const info = php.parse(data) as memo.ProgressBatch;
+        return {
+          progress: {
+            batch: {
+              epsTotal: info.eps_total,
+              epsUpdate: info.eps_update,
+              volsTotal: info.vols_total,
+              volsUpdate: info.vols_update,
+              subjectID: Number(info.subject_id),
+              subjectName: info.subject_name,
+            },
+          },
+        };
+      } else {
+        const info = php.parse(data) as memo.ProgressSingle;
+        return {
+          progress: {
+            single: {
+              epID: Number(info.ep_id),
+              epName: info.ep_name,
+              epSort: Number(info.ep_sort),
+              subjectID: Number(info.subject_id),
+              subjectName: info.subject_name,
+            },
+          },
+        };
+      }
+    }
+    case TimelineCat.Status: {
+      switch (type) {
+        case 0: {
+          return {
+            status: {
+              sign: data,
+            },
+          };
+        }
+        case 1: {
+          return {
+            status: {
+              tsukkomi: data,
+            },
+          };
+        }
+        case 2: {
+          const info = php.parse(data) as memo.Nickname;
+          return {
+            status: {
+              nickname: {
+                before: info.before,
+                after: info.after,
+              },
+            },
+          };
+        }
+        default: {
+          return {};
+        }
+      }
+    }
+    case TimelineCat.Blog: {
+      const info = php.parse(data) as memo.Blog;
+      return {
+        blog: {
+          id: Number(info.entry_id),
+          title: info.entry_title,
+          desc: info.entry_desc,
+        },
+      };
+    }
+    case TimelineCat.Index: {
+      const info = php.parse(data) as memo.Index;
+      return {
+        index: {
+          id: Number(info.idx_id),
+          title: info.idx_title,
+          desc: info.idx_desc,
+        },
+      };
+    }
+    case TimelineCat.Mono: {
+      const monos = [];
+      if (batch) {
+        const info = php.parse(data) as memo.MonoBatch;
+        for (const [_, value] of Object.entries(info)) {
+          monos.push(value);
+        }
+      } else {
+        const info = php.parse(data) as memo.MonoSingle;
+        monos.push(info);
+      }
+      return {
+        mono: monos.map((m) => ({
+          cat: m.cat,
+          id: m.id,
+          name: m.name,
+        })),
+      };
+    }
+    case TimelineCat.Doujin: {
+      return {};
+    }
+  }
+}
 
 export function toTimeline(tml: orm.ITimeline): res.ITimeline {
   return {
