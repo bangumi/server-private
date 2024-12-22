b06ad5d5a9785901d460dd4e7c7490cf37773caa
everpcpc <git@everpcpc.com>
2024-12-22 12:29:03 +0800

    fix: trim before slice

--- lib/types/convert.ts
+++ lib/types/convert.ts
@@ -349,7 +349,7 @@ export function toSlimBlogEntry(entry: orm.IBlogEntry): res.ISlimBlogEntry {
     id: entry.id,
     type: entry.type,
     title: entry.title,
-    summary: entry.content.slice(0, 120).replaceAll('\n', ' ').trim(),
+    summary: entry.content.replaceAll('\n', ' ').trim().slice(0, 120),
     replies: entry.replies,
     createdAt: entry.createdAt,
     updatedAt: entry.updatedAt,
