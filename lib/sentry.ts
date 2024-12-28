37d5ae160feaee63272268291fb52efdd588494e
Trim21 <trim21.me@gmail.com>
2024-12-28 17:38:28 +0800

    chore: improve sentry report

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,9 +1,28 @@
 import * as Sentry from '@sentry/node';
+import { DrizzleError } from 'drizzle-orm';
+import type { FastifyError } from 'fastify';
+import { TypeORMError } from 'typeorm';
 
 import config from '@app/lib/config.ts';
 
 if (config.sentryDSN) {
   Sentry.init({
     dsn: config.sentryDSN,
+    includeLocalVariables: true,
+    beforeSend(event, hint) {
+      const error = hint.originalException as FastifyError;
+      if (
+        typeof error.statusCode !== 'number' ||
+        error.statusCode === 500 ||
+        error instanceof TypeORMError ||
+        error instanceof DrizzleError
+      ) {
+        // drop expected and handled errors,
+        // like anything below code 500
+        return event;
+      }
+
+      return null;
+    },
   });
 }
