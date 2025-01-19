d82634ae82190f6db9474be8ac3c00406b5d870d
everpcpc <git@everpcpc.com>
2025-01-19 11:37:55 +0800

    z

--- routes/private/routes/collection.test.ts
+++ routes/private/routes/collection.test.ts
@@ -5,37 +5,4 @@ import { emptyAuth } from '@app/lib/auth/index.ts';
 
 import { setup } from './collection.ts';
 
-describe('episode collection', () => {
-  test('should get episodes', async () => {
-    const app = createTestServer({
-      auth: {
-        ...emptyAuth(),
-        login: true,
-        userID: 382951,
-      },
-    });
-    await app.register(setup);
-    const res = await app.inject({
-      method: 'get',
-      url: '/collections/subjects/2703/episodes',
-      query: { limit: '2', offset: '0' },
-    });
-    expect(res.json()).toMatchSnapshot();
-  });
-
-  test('should get single episode', async () => {
-    const app = createTestServer({
-      auth: {
-        ...emptyAuth(),
-        login: true,
-        userID: 382951,
-      },
-    });
-    await app.register(setup);
-    const res = await app.inject({
-      method: 'get',
-      url: '/collections/subjects/-/episodes/17227',
-    });
-    expect(res.json()).toMatchSnapshot();
-  });
-});
+describe('subject collection', () => {});
