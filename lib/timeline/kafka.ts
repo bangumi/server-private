f913d9fb22704ebfcbfbac4c31924822c4ef64b7
everpcpc <git@everpcpc.com>
2025-02-09 09:28:33 +0800

    z

--- lib/timeline/kafka.ts
+++ lib/timeline/kafka.ts
@@ -1,4 +1,5 @@
 import { logger } from '@app/lib/logger';
+import type { EpisodeCollectionStatus } from '@app/lib/subject/type';
 
 import { TimelineWriter } from './writer';
 
@@ -10,6 +11,44 @@ export async function handleTimelineMessage(op: string, details: string) {
         subjectID: number;
       };
       await TimelineWriter.subject(payload.userID, payload.subjectID);
+      break;
+    }
+    case 'progressEpisode': {
+      const payload = JSON.parse(details) as {
+        userID: number;
+        subjectID: number;
+        episodeID: number;
+        status: EpisodeCollectionStatus;
+      };
+      await TimelineWriter.progressEpisode(
+        payload.userID,
+        payload.subjectID,
+        payload.episodeID,
+        payload.status,
+      );
+      break;
+    }
+    case 'progressSubject': {
+      const payload = JSON.parse(details) as {
+        userID: number;
+        subjectID: number;
+        epsUpdate?: number;
+        volsUpdate?: number;
+      };
+      await TimelineWriter.progressSubject(
+        payload.userID,
+        payload.subjectID,
+        payload.epsUpdate,
+        payload.volsUpdate,
+      );
+      break;
+    }
+    case 'statusTsukkomi': {
+      const payload = JSON.parse(details) as {
+        userID: number;
+        text: string;
+      };
+      await TimelineWriter.statusTsukkomi(payload.userID, payload.text);
       break;
     }
     default: {
