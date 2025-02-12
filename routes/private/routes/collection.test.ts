5fc01c253f14649193921bacc76480bb90ed8a41
everpcpc <git@everpcpc.com>
2025-02-12 15:48:53 +0800

    fix: update subject fields for rating

--- routes/private/routes/collection.test.ts
+++ routes/private/routes/collection.test.ts
@@ -1,11 +1,63 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
+import { db, op } from '@app/drizzle/db.ts';
+import * as schema from '@app/drizzle/schema.ts';
 import { createTestServer } from '@app/tests/utils.ts';
 import { emptyAuth } from '@app/lib/auth/index.ts';
+import { CollectionPrivacy, CollectionType } from '@app/lib/subject/type.ts';
 
 import { setup } from './collection.ts';
 
 describe('subject collection', () => {
+  beforeEach(async () => {
+    await db
+      .delete(schema.chiiSubjectInterests)
+      .where(
+        op.and(
+          op.eq(schema.chiiSubjectInterests.uid, 382951),
+          op.eq(schema.chiiSubjectInterests.subjectID, 12),
+        ),
+      );
+    // reset collection count
+    await db
+      .update(schema.chiiSubjects)
+      .set({
+        doing: 215,
+      })
+      .where(op.eq(schema.chiiSubjects.id, 12));
+    // reset rating count
+    await db
+      .update(schema.chiiSubjectFields)
+      .set({
+        rate10: 168,
+      })
+      .where(op.eq(schema.chiiSubjectFields.id, 12));
+  });
+
+  afterEach(async () => {
+    await db
+      .delete(schema.chiiSubjectInterests)
+      .where(
+        op.and(
+          op.eq(schema.chiiSubjectInterests.uid, 382951),
+          op.eq(schema.chiiSubjectInterests.subjectID, 12),
+        ),
+      );
+    // reset collection count
+    await db
+      .update(schema.chiiSubjects)
+      .set({
+        doing: 215,
+      })
+      .where(op.eq(schema.chiiSubjects.id, 12));
+    // reset rating count
+    await db
+      .update(schema.chiiSubjectFields)
+      .set({
+        rate10: 168,
+      })
+      .where(op.eq(schema.chiiSubjectFields.id, 12));
+  });
   test('should get subject collections', async () => {
     const app = createTestServer({
       auth: {
@@ -21,6 +73,52 @@ describe('subject collection', () => {
       query: { limit: '2', offset: '0' },
     });
     expect(res.json()).toMatchSnapshot();
+  });
+
+  test('should create and edit subject collection', async () => {
+    const app = createTestServer({
+      auth: {
+        ...emptyAuth(),
+        login: true,
+        userID: 382951,
+      },
+    });
+    await app.register(setup);
+    const res = await app.inject({
+      method: 'put',
+      url: '/collections/subjects/12',
+      body: {
+        type: CollectionType.Doing,
+        rate: 10,
+        comment: 'test',
+        private: false,
+      },
+    });
+    expect(res.statusCode).toBe(200);
+    const [collection] = await db
+      .select()
+      .from(schema.chiiSubjectInterests)
+      .where(
+        op.and(
+          op.eq(schema.chiiSubjectInterests.uid, 382951),
+          op.eq(schema.chiiSubjectInterests.subjectID, 12),
+        ),
+      );
+    expect(collection).toBeDefined();
+    expect(collection?.type).toBe(CollectionType.Doing);
+    expect(collection?.rate).toBe(10);
+    expect(collection?.comment).toBe('test');
+    expect(collection?.privacy).toBe(CollectionPrivacy.Public);
+    const [subject] = await db
+      .select()
+      .from(schema.chiiSubjects)
+      .where(op.eq(schema.chiiSubjects.id, 12));
+    expect(subject?.doing).toBe(216);
+    const [subjectFields] = await db
+      .select()
+      .from(schema.chiiSubjectFields)
+      .where(op.eq(schema.chiiSubjectFields.id, 12));
+    expect(subjectFields?.rate10).toBe(169);
   });
 });
 
