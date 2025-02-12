bfee27c7c76fc09a07ae1deb3485a697d8b22f14
everpcpc <git@everpcpc.com>
2025-02-12 09:57:21 +0800

    z

--- lib/comment.ts
+++ lib/comment.ts
@@ -167,7 +167,11 @@ export class CommentWithoutState {
   }
 
   async getAll(mainID: number) {
-    const data = await db.select().from(this.table).where(op.eq(this.table.mid, mainID));
+    const data = await db
+      .select()
+      .from(this.table)
+      .where(op.eq(this.table.mid, mainID))
+      .orderBy(op.asc(this.table.id));
     const uids = data.map((v) => v.uid);
     const users = await fetcher.fetchSlimUsersByIDs(uids);
     const comments: res.IComment[] = [];
