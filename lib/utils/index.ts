9be21c9f37423547e6850295ae7413874d371537
everpcpc <everpcpc@users.noreply.github.com>
2025-02-14 20:26:43 +0800

    feat: add cron to clean access tokens (#1050)
    Co-authored-by: Trim21 <trim21.me@gmail.com>

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -169,3 +169,7 @@ function formatLongDuration(durationSeconds: number) {
 
   return s.join('');
 }
+
+export async function sleep(ms: number) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
