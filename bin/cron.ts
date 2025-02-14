5a830769f3f48a76d3d76defa5369da5bd7d6848
everpcpc <git@everpcpc.com>
2025-02-14 21:07:40 +0800

    fix: add errorHandler for cronjob

--- bin/cron.ts
+++ bin/cron.ts
@@ -19,22 +19,51 @@ import { trendingSubjects } from '@app/tasks/trending';
 // month          1-12 (or names, see below)
 // day of week    0-7 (0 or 7 is Sunday, or use names)
 
-// eslint-disable-next-line @typescript-eslint/require-await
-async function main() {
-  const jobs: Record<string, CronJob> = {
-    heartbeat: new CronJob('*/10 * * * * *', heartbeat),
-    trendingSubjects: new CronJob('0 0 19 * * *', trendingSubjects),
-    truncateTimelineGlobalCache: new CronJob('*/10 * * * *', truncateTimelineGlobalCache),
-    truncateTimelineInboxCache: new CronJob('0 0 20 * * *', truncateTimelineInboxCache),
-    truncateTimelineUserCache: new CronJob('0 0 21 * * *', truncateTimelineUserCache),
-    cleanupExpiredAccessTokens: new CronJob('0 0 22 * * *', cleanupExpiredAccessTokens),
-    cleanupExpiredRefreshTokens: new CronJob('0 0 23 * * *', cleanupExpiredRefreshTokens),
-  };
+function newCronJob(name: string, cronTime: string, onTick: () => Promise<void>): CronJob {
+  return CronJob.from({
+    cronTime,
+    onTick,
+    timeZone: 'Asia/Shanghai',
+    errorHandler: (error) => {
+      logger.error(`Cronjob ${name} failed: ${error}`);
+    },
+  });
+}
 
+function main() {
+  const jobs: Record<string, CronJob> = {
+    heartbeat: newCronJob('heartbeat', '*/10 * * * * *', heartbeat),
+    trendingSubjects: newCronJob('trendingSubjects', '0 0 3 * * *', trendingSubjects),
+    truncateTimelineGlobalCache: newCronJob(
+      'truncateTimelineGlobalCache',
+      '*/10 * * * *',
+      truncateTimelineGlobalCache,
+    ),
+    truncateTimelineInboxCache: newCronJob(
+      'truncateTimelineInboxCache',
+      '0 0 4 * * *',
+      truncateTimelineInboxCache,
+    ),
+    truncateTimelineUserCache: newCronJob(
+      'truncateTimelineUserCache',
+      '0 0 5 * * *',
+      truncateTimelineUserCache,
+    ),
+    cleanupExpiredAccessTokens: newCronJob(
+      'cleanupExpiredAccessTokens',
+      '0 0 6 * * *',
+      cleanupExpiredAccessTokens,
+    ),
+    cleanupExpiredRefreshTokens: newCronJob(
+      'cleanupExpiredRefreshTokens',
+      '0 0 7 * * *',
+      cleanupExpiredRefreshTokens,
+    ),
+  };
   for (const [name, job] of Object.entries(jobs)) {
     logger.info(`Cronjob: ${name} @ ${job.cronTime.source}`);
     job.start();
   }
 }
 
-await main();
+main();
