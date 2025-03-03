ec01e429a814cca68be53c05e37113c3c7d8c3a1
everpcpc <git@everpcpc.com>
2025-03-03 09:05:31 +0800

    z

--- lib/like.ts
+++ lib/like.ts
@@ -109,101 +109,134 @@ function validateReaction(type: LikeType, value: number): boolean {
   }
 }
 
-async function fetchReactionsByMainID(
-  mainID: number,
-  type: LikeType,
-): Promise<Record<number, res.IReaction[]>> {
-  if (!ALLOWED_REACTION_TYPES.has(type)) {
-    throw new BadRequestError('Invalid reaction type');
-  }
-
-  const data = await db
-    .select()
-    .from(schema.chiiLikes)
-    .where(
-      op.and(
-        op.eq(schema.chiiLikes.mainID, mainID),
-        op.eq(schema.chiiLikes.type, type),
-        op.eq(schema.chiiLikes.deleted, false),
-      ),
-    );
-
-  const uids = data.map((x) => x.uid);
-  const users = await fetcher.fetchSimpleUsersByIDs(uids);
-  const r = lo.groupBy(data, (x) => x.relatedID);
-
-  return lo.mapValues(r, (v): res.IReaction[] => {
-    return Object.entries(
-      lo.groupBy(v, (a) => {
-        return a.value;
-      }),
-    ).map(([key, values]) => {
-      return {
-        users: values.map((x) => users[x.uid]).filter((x) => x !== undefined),
-        value: Number(key),
-      };
-    });
-  });
-}
-
-async function fetchReactionsByRelatedIDs(
-  type: LikeType,
-  relatedIDs: number[],
-): Promise<Record<number, res.IReaction[]>> {
-  const data = await db
-    .select()
-    .from(schema.chiiLikes)
-    .where(
-      op.and(
-        op.eq(schema.chiiLikes.type, type),
-        op.inArray(schema.chiiLikes.relatedID, relatedIDs),
-        op.eq(schema.chiiLikes.deleted, false),
-      ),
-    );
-
-  const uids = data.map((x) => x.uid);
-  const users = await fetcher.fetchSimpleUsersByIDs(uids);
-  const r = lo.groupBy(data, (x) => x.relatedID);
-
-  return lo.mapValues(r, (v): res.IReaction[] => {
-    return Object.entries(
-      lo.groupBy(v, (a) => {
-        return a.value;
-      }),
-    ).map(([key, values]) => {
-      return {
-        users: values.map((x) => users[x.uid]).filter((x) => x !== undefined),
-        value: Number(key),
-      };
-    });
-  });
-}
-
-async function addReaction(reaction: NewReaction) {
-  if (!validateReaction(reaction.type, reaction.value)) {
-    throw new BadRequestError('Invalid reaction');
-  }
-  await rateLimit(LimitAction.Like, reaction.uid);
-  await db.transaction(async (tx) => {
-    const [previous] = await tx
+export const Reaction = {
+  async fetchByMainID(mainID: number, type: LikeType): Promise<Record<number, res.IReaction[]>> {
+    if (!ALLOWED_REACTION_TYPES.has(type)) {
+      throw new BadRequestError('Invalid reaction type');
+    }
+    const data = await db
       .select()
       .from(schema.chiiLikes)
       .where(
         op.and(
-          op.eq(schema.chiiLikes.type, reaction.type),
-          op.eq(schema.chiiLikes.relatedID, reaction.rid),
-          op.eq(schema.chiiLikes.uid, reaction.uid),
+          op.eq(schema.chiiLikes.mainID, mainID),
+          op.eq(schema.chiiLikes.type, type),
+          op.eq(schema.chiiLikes.deleted, false),
         ),
-      )
-      .limit(1);
-    if (previous) {
-      if (previous.value === reaction.value && !previous.deleted) {
-        return;
+      );
+    const uids = data.map((x) => x.uid);
+    const users = await fetcher.fetchSimpleUsersByIDs(uids);
+    const r = lo.groupBy(data, (x) => x.relatedID);
+    return lo.mapValues(r, (v): res.IReaction[] => {
+      return Object.entries(
+        lo.groupBy(v, (a) => {
+          return a.value;
+        }),
+      ).map(([key, values]) => {
+        return {
+          users: values.map((x) => users[x.uid]).filter((x) => x !== undefined),
+          value: Number(key),
+        };
+      });
+    });
+  },
+
+  async fetchByRelatedIDs(
+    type: LikeType,
+    relatedIDs: number[],
+  ): Promise<Record<number, res.IReaction[]>> {
+    const data = await db
+      .select()
+      .from(schema.chiiLikes)
+      .where(
+        op.and(
+          op.eq(schema.chiiLikes.type, type),
+          op.inArray(schema.chiiLikes.relatedID, relatedIDs),
+          op.eq(schema.chiiLikes.deleted, false),
+        ),
+      );
+    const uids = data.map((x) => x.uid);
+    const users = await fetcher.fetchSimpleUsersByIDs(uids);
+    const r = lo.groupBy(data, (x) => x.relatedID);
+    return lo.mapValues(r, (v): res.IReaction[] => {
+      return Object.entries(
+        lo.groupBy(v, (a) => {
+          return a.value;
+        }),
+      ).map(([key, values]) => {
+        return {
+          users: values.map((x) => users[x.uid]).filter((x) => x !== undefined),
+          value: Number(key),
+        };
+      });
+    });
+  },
+
+  async add(reaction: NewReaction) {
+    if (!validateReaction(reaction.type, reaction.value)) {
+      throw new BadRequestError('Invalid reaction');
+    }
+    await rateLimit(LimitAction.Like, reaction.uid);
+    await db.transaction(async (tx) => {
+      const [previous] = await tx
+        .select()
+        .from(schema.chiiLikes)
+        .where(
+          op.and(
+            op.eq(schema.chiiLikes.type, reaction.type),
+            op.eq(schema.chiiLikes.relatedID, reaction.rid),
+            op.eq(schema.chiiLikes.uid, reaction.uid),
+          ),
+        )
+        .limit(1);
+      if (previous) {
+        if (previous.value === reaction.value && !previous.deleted) {
+          return;
+        } else {
+          // 更新
+          await tx
+            .update(schema.chiiLikes)
+            .set({ deleted: false, value: reaction.value })
+            .where(
+              op.and(
+                op.eq(schema.chiiLikes.type, reaction.type),
+                op.eq(schema.chiiLikes.relatedID, reaction.rid),
+                op.eq(schema.chiiLikes.uid, reaction.uid),
+              ),
+            );
+        }
       } else {
-        // 更新
+        // 新增
+        await tx.insert(schema.chiiLikes).values({
+          type: reaction.type,
+          mainID: reaction.mid,
+          relatedID: reaction.rid,
+          uid: reaction.uid,
+          value: reaction.value,
+          createdAt: DateTime.now().toUnixInteger(),
+          deleted: false,
+        });
+      }
+    });
+  },
+
+  async delete(reaction: DeleteReaction) {
+    await db.transaction(async (tx) => {
+      const [previous] = await tx
+        .select()
+        .from(schema.chiiLikes)
+        .where(
+          op.and(
+            op.eq(schema.chiiLikes.type, reaction.type),
+            op.eq(schema.chiiLikes.relatedID, reaction.rid),
+            op.eq(schema.chiiLikes.uid, reaction.uid),
+          ),
+        )
+        .limit(1);
+      if (previous && !previous.deleted) {
         await tx
           .update(schema.chiiLikes)
-          .set({ deleted: false, value: reaction.value })
+          .set({ deleted: true })
           .where(
             op.and(
               op.eq(schema.chiiLikes.type, reaction.type),
@@ -212,52 +245,6 @@ async function addReaction(reaction: NewReaction) {
             ),
           );
       }
-    } else {
-      // 新增
-      await tx.insert(schema.chiiLikes).values({
-        type: reaction.type,
-        mainID: reaction.mid,
-        relatedID: reaction.rid,
-        uid: reaction.uid,
-        value: reaction.value,
-        createdAt: DateTime.now().toUnixInteger(),
-        deleted: false,
-      });
-    }
-  });
-}
-
-async function deleteReaction(reaction: DeleteReaction) {
-  await db.transaction(async (tx) => {
-    const [previous] = await tx
-      .select()
-      .from(schema.chiiLikes)
-      .where(
-        op.and(
-          op.eq(schema.chiiLikes.type, reaction.type),
-          op.eq(schema.chiiLikes.relatedID, reaction.rid),
-          op.eq(schema.chiiLikes.uid, reaction.uid),
-        ),
-      )
-      .limit(1);
-    if (previous && !previous.deleted) {
-      await tx
-        .update(schema.chiiLikes)
-        .set({ deleted: true })
-        .where(
-          op.and(
-            op.eq(schema.chiiLikes.type, reaction.type),
-            op.eq(schema.chiiLikes.relatedID, reaction.rid),
-            op.eq(schema.chiiLikes.uid, reaction.uid),
-          ),
-        );
-    }
-  });
-}
-
-export const Reaction = {
-  fetchByMainID: fetchReactionsByMainID,
-  fetchByRelatedIDs: fetchReactionsByRelatedIDs,
-  add: addReaction,
-  delete: deleteReaction,
+    });
+  },
 };
