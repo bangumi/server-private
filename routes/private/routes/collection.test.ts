e9c289de68e9e7392a2ed445b494d9e62920df00
everpcpc <git@everpcpc.com>
2025-03-03 09:57:00 +0800

    chore: add test for subject collect with progress

--- routes/private/routes/collection.test.ts
+++ routes/private/routes/collection.test.ts
@@ -16,10 +16,14 @@ async function resetSubjectInterest() {
         op.eq(schema.chiiSubjectInterests.subjectID, 12),
       ),
     );
+  await db
+    .delete(schema.chiiEpStatus)
+    .where(op.and(op.eq(schema.chiiEpStatus.uid, 382951), op.eq(schema.chiiEpStatus.sid, 12)));
   // reset collection count
   await db
     .update(schema.chiiSubjects)
     .set({
+      collect: 4534,
       doing: 215,
     })
     .where(op.eq(schema.chiiSubjects.id, 12));
@@ -58,7 +62,7 @@ describe('subject collection', () => {
     expect(res.json()).toMatchSnapshot();
   });
 
-  test('should create and edit subject collection', async () => {
+  test('should create subject collection', async () => {
     const app = createTestServer({
       auth: {
         ...emptyAuth(),
@@ -75,6 +79,7 @@ describe('subject collection', () => {
         rate: 10,
         comment: 'test',
         private: false,
+        progress: true,
       },
     });
     expect(res.statusCode).toBe(200);
@@ -89,6 +94,8 @@ describe('subject collection', () => {
       );
     expect(collection).toBeDefined();
     expect(collection?.type).toBe(CollectionType.Doing);
+    expect(collection?.epStatus).toBe(0);
+    expect(collection?.volStatus).toBe(0);
     expect(collection?.rate).toBe(10);
     expect(collection?.comment).toBe('test');
     expect(collection?.privacy).toBe(CollectionPrivacy.Public);
@@ -102,6 +109,45 @@ describe('subject collection', () => {
       .from(schema.chiiSubjectFields)
       .where(op.eq(schema.chiiSubjectFields.id, 12));
     expect(subjectFields?.rate10).toBe(169);
+  });
+
+  test('should create subject collection with progress', async () => {
+    const app = createTestServer({
+      auth: {
+        ...emptyAuth(),
+        login: true,
+        userID: 382951,
+      },
+    });
+    await app.register(setup);
+    const res = await app.inject({
+      method: 'put',
+      url: '/collections/subjects/12',
+      body: {
+        type: CollectionType.Collect,
+        progress: true,
+      },
+    });
+    expect(res.statusCode).toBe(200);
+    const [collection] = await db
+      .select()
+      .from(schema.chiiSubjectInterests)
+      .where(
+        op.and(
+          op.eq(schema.chiiSubjectInterests.uid, 382951),
+          op.eq(schema.chiiSubjectInterests.subjectID, 12),
+        ),
+      );
+    expect(collection).toBeDefined();
+    expect(collection?.type).toBe(CollectionType.Collect);
+    expect(collection?.epStatus).toBe(27);
+    expect(collection?.volStatus).toBe(0);
+    expect(collection?.privacy).toBe(CollectionPrivacy.Public);
+    const [subject] = await db
+      .select()
+      .from(schema.chiiSubjects)
+      .where(op.eq(schema.chiiSubjects.id, 12));
+    expect(subject?.collect).toBe(4535);
   });
 });
 
