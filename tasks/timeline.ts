4196c63331ca643df302f4fb89d10a5012858a17
everpcpc <everpcpc@users.noreply.github.com>
2025-04-07 11:59:25 +0800

    chore: add logs for truncate timeline cache (#1190)

--- tasks/timeline.ts
+++ tasks/timeline.ts
@@ -5,6 +5,7 @@ import { getInboxCacheKey, getUserCacheKey } from '@app/lib/timeline/cache';
 export async function truncateGlobalCache() {
   logger.info('Truncating global timeline cache...');
   const cacheKey = getInboxCacheKey(0);
+  logger.info(`Truncating global timeline cache with key: ${cacheKey}`);
   await redis.zremrangebyrank(cacheKey, 0, -1001);
 }
 
@@ -12,6 +13,7 @@ export async function truncateUserCache() {
   logger.info('Truncating user timeline cache...');
   const keys = redis.scanStream({ match: getUserCacheKey('*'), type: 'zset' });
   for await (const key of keys) {
+    logger.info(`Truncating user timeline cache with key: ${key}`);
     await redis.zremrangebyrank(key as string, 0, -201);
   }
 }
@@ -20,6 +22,7 @@ export async function truncateInboxCache() {
   logger.info('Truncating inbox timeline cache...');
   const keys = redis.scanStream({ match: getInboxCacheKey('*'), type: 'zset' });
   for await (const key of keys) {
+    logger.info(`Truncating inbox timeline cache with key: ${key}`);
     await redis.zremrangebyrank(key as string, 0, -201);
   }
 }
