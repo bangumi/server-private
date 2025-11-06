ef0493a238403cde7edb11bf6f2c60efd9bea4e4
inchei <inchei@protonmail.com>
2025-11-06 21:29:16 +0800

    feat: add person wiki history related apis

--- routes/private/routes/wiki/person/index.ts
+++ routes/private/routes/wiki/person/index.ts
@@ -10,8 +10,10 @@ import { createRevision, RevType } from '@app/lib/orm/entity/index.ts';
 import * as entity from '@app/lib/orm/entity/index.ts';
 import { AppDataSource } from '@app/lib/orm/index.ts';
 import { InvalidWikiSyntaxError } from '@app/lib/subject/index.ts';
+import * as fetcher from '@app/lib/types/fetcher.ts';
 import * as res from '@app/lib/types/res.ts';
 import { formatErrors } from '@app/lib/types/res.ts';
+import { ghostUser } from '@app/lib/user/utils';
 import { matchExpected, WikiChangedError } from '@app/lib/wiki.ts';
 import { requireLogin } from '@app/routes/hooks/pre-handler.ts';
 import type { App } from '@app/routes/type.ts';
@@ -54,10 +56,59 @@ export const PersonEdit = t.Object(
   },
 );
 
+type IPersonHistorySummary = Static<typeof PersonHistorySummary>;
+export const PersonHistorySummary = t.Object(
+  {
+    id: t.Integer(),
+    creator: t.Object({
+      username: t.String(),
+    }),
+    type: t.Integer({
+      description: '3 = 人物编辑，15 = 合并，16 = 删除',
+    }),
+    commitMessage: t.String(),
+    createdAt: t.Integer({ description: 'unix timestamp seconds' }),
+  },
+  { $id: 'PersonHistorySummary' },
+);
+
+type IPersonRevisionWikiInfo = Static<typeof PersonRevisionWikiInfo>;
+export const PersonRevisionWikiInfo = t.Object(
+  {
+    name: t.String(),
+    infobox: t.String(),
+    summary: t.String(),
+    extra: t.Object({
+      img: t.Optional(t.String()),
+    }),
+  },
+  {
+    $id: 'PersonRevisionWikiInfo',
+  },
+);
+
+type IUserPersonContribution = Static<typeof UserPersonContribution>;
+const UserPersonContribution = t.Object(
+  {
+    id: t.Integer(),
+    type: t.Integer({
+      description: '3 = 人物编辑，15 = 合并，16 = 删除',
+    }),
+    personID: t.Integer(),
+    name: t.String(),
+    commitMessage: t.String(),
+    createdAt: t.Integer({ description: 'unix timestamp seconds' }),
+  },
+  { $id: 'UserPersonContribution' },
+);
+
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function setup(app: App) {
   app.addSchema(res.PersonType);
   app.addSchema(PersonWikiInfo);
+  app.addSchema(PersonHistorySummary);
+  app.addSchema(PersonRevisionWikiInfo);
+  app.addSchema(UserPersonContribution);
 
   app.get(
     '/persons/:personID',
@@ -198,6 +249,195 @@ export async function setup(app: App) {
       });
 
       return {};
+    },
+  );
+
+  app.get(
+    '/persons/:personID/history-summary',
+    {
+      schema: {
+        tags: [Tag.Wiki],
+        operationId: 'subjectEditHistorySummary',
+        summary: '获取人物 wiki 历史编辑摘要',
+        params: t.Object({
+          personID: t.Integer({ minimum: 1 }),
+        }),
+        querystring: t.Object({
+          limit: t.Optional(
+            t.Integer({ default: 20, minimum: 1, maximum: 100, description: 'max 100' }),
+          ),
+          offset: t.Optional(t.Integer({ default: 0, minimum: 0, description: 'min 0' })),
+        }),
+        security: [{ [Security.CookiesSession]: [], [Security.HTTPBearer]: [] }],
+        response: {
+          200: res.Paged(res.Ref(PersonHistorySummary)),
+        },
+      },
+    },
+    async ({ params: { personID }, query: { limit = 20, offset = 0 } }) => {
+      const [{ count = 0 } = {}] = await db
+        .select({ count: op.countDistinct(schema.chiiRevHistory.revId) })
+        .from(schema.chiiRevHistory)
+        .where(
+          op.and(
+            op.eq(schema.chiiRevHistory.revMid, personID),
+            op.inArray(schema.chiiRevHistory.revType, PersonEditTypes),
+          ),
+        );
+
+      const history = await db
+        .select()
+        .from(schema.chiiRevHistory)
+        .where(
+          op.and(
+            op.eq(schema.chiiRevHistory.revMid, personID),
+            op.inArray(schema.chiiRevHistory.revType, PersonEditTypes),
+          ),
+        )
+        .orderBy(op.desc(schema.chiiRevHistory.revId))
+        .offset(offset)
+        .limit(limit);
+
+      const users = await fetcher.fetchSlimUsersByIDs(history.map((x) => x.revCreator));
+
+      const revisions = history.map((x) => {
+        return {
+          id: x.revId,
+          creator: {
+            username: users[x.revCreator]?.username ?? ghostUser(x.revCreator).username,
+          },
+          type: x.revType,
+          createdAt: x.createdAt,
+          commitMessage: x.revEditSummary,
+        } satisfies IPersonHistorySummary;
+      });
+
+      return {
+        total: count,
+        data: revisions,
+      };
+    },
+  );
+
+  app.get(
+    '/persons/revisions/:revisionID',
+    {
+      schema: {
+        tags: [Tag.Wiki],
+        operationId: 'getPersonRevisionInfo',
+        summary: '获取人物历史版本 wiki 信息',
+        params: t.Object({
+          revisionID: t.Integer({ minimum: 1 }),
+        }),
+        security: [{ [Security.CookiesSession]: [], [Security.HTTPBearer]: [] }],
+        response: {
+          200: res.Ref(PersonRevisionWikiInfo),
+          404: res.Ref(res.Error, {
+            'x-examples': formatErrors(new NotFoundError('revision')),
+          }),
+        },
+      },
+    },
+    async ({ params: { revisionID } }): Promise<IPersonRevisionWikiInfo> => {
+      const [r] = await db
+        .select()
+        .from(schema.chiiRevHistory)
+        .where(op.eq(schema.chiiRevHistory.revId, revisionID));
+      if (!r) {
+        throw new NotFoundError(`revision ${revisionID}`);
+      }
+
+      const [revText] = await db
+        .select()
+        .from(schema.chiiRevText)
+        .where(op.eq(schema.chiiRevText.revTextId, r.revTextId));
+      if (!revText) {
+        throw new NotFoundError(`RevText ${r.revTextId}`);
+      }
+
+      const revRecord = await entity.RevText.deserialize(revText.revText);
+      const revContent = revRecord[revisionID] as PersonRev;
+
+      return {
+        name: revContent.prsn_name,
+        infobox: revContent.prsn_infobox,
+        summary: revContent.prsn_summary,
+        extra: revContent.extra,
+      };
+    },
+  );
+
+  app.get(
+    '/users/:username/contributions/persons',
+    {
+      schema: {
+        tags: [Tag.Wiki],
+        operationId: 'getUserContributedPersons',
+        summary: '获取用户 wiki 人物编辑记录',
+        params: t.Object({
+          username: t.String(),
+        }),
+        querystring: t.Object({
+          limit: t.Optional(
+            t.Integer({ default: 20, minimum: 1, maximum: 100, description: 'max 100' }),
+          ),
+          offset: t.Optional(t.Integer({ default: 0, minimum: 0, description: 'min 0' })),
+        }),
+        security: [{ [Security.CookiesSession]: [], [Security.HTTPBearer]: [] }],
+        response: {
+          200: res.Paged(res.Ref(UserPersonContribution)),
+          404: res.Ref(res.Error, {
+            'x-examples': formatErrors(new NotFoundError('user')),
+          }),
+        },
+      },
+    },
+    async ({ params: { username }, query: { limit = 20, offset = 0 } }) => {
+      const user = await fetcher.fetchSlimUserByUsername(username);
+      if (!user) {
+        throw new NotFoundError('user');
+      }
+
+      const [{ count = 0 } = {}] = await db
+        .select({ count: op.countDistinct(schema.chiiRevHistory.revId) })
+        .from(schema.chiiRevHistory)
+        .where(
+          op.and(
+            op.eq(schema.chiiRevHistory.revCreator, user.id),
+            op.inArray(schema.chiiRevHistory.revType, PersonEditTypes),
+          ),
+        );
+
+      const history = await db
+        .select()
+        .from(schema.chiiRevHistory)
+        .where(
+          op.and(
+            op.eq(schema.chiiRevHistory.revCreator, user.id),
+            op.inArray(schema.chiiRevHistory.revType, PersonEditTypes),
+          ),
+        )
+        .orderBy(op.desc(schema.chiiRevHistory.revId))
+        .offset(offset)
+        .limit(limit);
+
+      const persons = await fetcher.fetchSlimPersonsByIDs(history.map((r) => r.revMid));
+
+      const revisions = history.map((r) => {
+        return {
+          id: r.revId,
+          type: r.revType,
+          personID: r.revMid,
+          name: persons[r.revMid]?.name || String(r.revMid),
+          commitMessage: r.revEditSummary,
+          createdAt: r.createdAt,
+        } satisfies IUserPersonContribution;
+      });
+
+      return {
+        total: count,
+        data: revisions,
+      };
     },
   );
 }
