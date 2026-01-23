1a2928c98ab73af7daf0dc36ff57b7c79a93389b
why <wanghuaiyv@gmail.com>
2026-01-23 19:37:53 +0800

    fix: timeline

--- event/timeline.ts
+++ event/timeline.ts
@@ -42,14 +42,14 @@ export async function handle({ key, value }: KafkaMessage) {
         return;
       }
 
-      async function createTimeline(tml: TimelineItem) {
+      async function cacheTimeline(tml: TimelineItem, cacheKeyCat: number) {
         // 始终写入全站时间线
-        await redis.zadd(getInboxCacheKey(0, tml.tml_cat), tml.tml_id, tml.tml_id);
+        await redis.zadd(getInboxCacheKey(0, cacheKeyCat), tml.tml_id, tml.tml_id);
 
         const now = DateTime.now().toUnixInteger();
-        const ttlUser = Number(await redis.get(getUserVisitCacheKey(tml.tml_uid, tml.tml_cat)));
+        const ttlUser = Number(await redis.get(getUserVisitCacheKey(tml.tml_uid, cacheKeyCat)));
         if (ttlUser > 0) {
-          const userCacheKey = getUserCacheKey(tml.tml_uid, tml.tml_cat);
+          const userCacheKey = getUserCacheKey(tml.tml_uid, cacheKeyCat);
           await redis.zadd(userCacheKey, tml.tml_id, tml.tml_id);
           // 将 cache key 的过期时间设置为与 visit key 一致
           await redis.expire(userCacheKey, ttlUser - now);
@@ -59,18 +59,18 @@ export async function handle({ key, value }: KafkaMessage) {
           // 写入自己的首页时间线
           followerIDs.push(tml.tml_uid);
           const ttlInbox = await redis.mget(
-            followerIDs.map((fid) => getInboxVisitCacheKey(fid, tml.tml_cat)),
+            followerIDs.map((fid) => getInboxVisitCacheKey(fid, cacheKeyCat)),
           );
           for (const [idx, fid] of followerIDs.entries()) {
             const ttl = Number(ttlInbox[idx]);
             if (ttl > 0) {
-              const inboxCacheKey = getInboxCacheKey(fid, tml.tml_cat);
+              const inboxCacheKey = getInboxCacheKey(fid, cacheKeyCat);
               await redis.zadd(inboxCacheKey, tml.tml_id, tml.tml_id);
               await redis.expire(inboxCacheKey, ttl - now);
             }
           }
         }
-        if (tml.tml_cat != 0) {
+        if (cacheKeyCat != 0) {
           await redis.publish(
             TIMELINE_EVENT_CHANNEL,
             JSON.stringify({ tml_id: tml.tml_id, cat: tml.tml_cat, uid: tml.tml_uid }),
@@ -78,11 +78,8 @@ export async function handle({ key, value }: KafkaMessage) {
         }
       }
       const tml = payload.after;
-      await createTimeline(tml);
-
-      // 缓存未指定 cat 的副本
-      const tmlCat0 = { ...tml, cat: 0 };
-      await createTimeline(tmlCat0);
+      await cacheTimeline(tml, 0);
+      await cacheTimeline(tml, tml.tml_cat);
       break;
     }
     case EventOp.Delete: {
@@ -90,28 +87,26 @@ export async function handle({ key, value }: KafkaMessage) {
         logger.error({ payload }, 'invalid timeline payload for delete');
         return;
       }
-      async function deleteTimeline(tml: TimelineItem) {
+      async function deleteTimeline(tml: TimelineItem, cacheKeyCat: number) {
         logger.info(`process timeline delete event: ${tml.tml_id}`);
         // 始终尝试从全站时间线中删除
-        await redis.zrem(getInboxCacheKey(0, tml.tml_cat), tml.tml_id);
+        await redis.zrem(getInboxCacheKey(0, cacheKeyCat), tml.tml_id);
 
-        await redis.zrem(getUserCacheKey(tml.tml_uid, tml.tml_cat), tml.tml_id);
+        await redis.zrem(getUserCacheKey(tml.tml_uid, cacheKeyCat), tml.tml_id);
         const followerIDs = await fetchFollowers(tml.tml_uid);
         if (followerIDs.length > 0) {
           // 也从自己的首页时间线中删除
           followerIDs.push(tml.tml_uid);
           for (const fid of followerIDs) {
-            await redis.zrem(getInboxCacheKey(fid, tml.tml_cat), tml.tml_id);
+            await redis.zrem(getInboxCacheKey(fid, cacheKeyCat), tml.tml_id);
           }
         }
         await redis.del(getItemCacheKey(idx.tml_id));
       }
-      const tml = payload.before;
-      await deleteTimeline(tml);
 
-      // 删除未指定 cat 的副本
-      const tmlCat0 = { ...tml, cat: 0 };
-      await deleteTimeline(tmlCat0);
+      const tml = payload.before;
+      await deleteTimeline(tml, 0);
+      await deleteTimeline(tml, tml.tml_cat);
       break;
     }
     case EventOp.Update: {
