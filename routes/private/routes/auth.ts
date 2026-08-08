8b402ccb72af93197f1fe3c133c65e4167eeb6a9
shqingda <shqingda@gmail.com>
2026-08-08 13:18:01 +0800

    Allow Kaku Turnstile callback

--- routes/private/routes/auth.ts
+++ routes/private/routes/auth.ts
@@ -42,6 +42,8 @@ const allowedRedirectUris: string[] = [
   'https://oauth-backend-jet.vercel.app/api/turnstile/callback',
   // https://github.com/gi-b716/AniMikan
   'animikan://api/bangumi/turnstile/callback',
+  // https://github.com/shqingda/kaku
+  'kaku://auth/turnstile',
 ];
 
 // eslint-disable-next-line @typescript-eslint/require-await
