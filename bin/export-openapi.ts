91f5eed55d71105654e669983f02ebc9937a1915
Trim21 <trim21.me@gmail.com>
2026-08-13 21:48:03 +0800

    feat(api): add index list API (#1778)

--- bin/export-openapi.ts
+++ bin/export-openapi.ts
@@ -2,6 +2,7 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
 import * as lodash from 'lodash-es';
+import { format, resolveConfig } from 'prettier';
 
 import { projectRoot } from '@app/lib/config.ts';
 import { createServer } from '@app/lib/server.ts';
@@ -9,7 +10,10 @@ import { createServer } from '@app/lib/server.ts';
 const app = await createServer();
 
 const pri = await app.inject('/p1/openapi.json');
-await fs.writeFile(
-  path.resolve(projectRoot, 'openapi.json'),
-  JSON.stringify(lodash.omit(JSON.parse(pri.body), 'info.version'), null, 2),
-);
+const outFile = path.resolve(projectRoot, 'openapi.json');
+
+const body = JSON.stringify(lodash.omit(JSON.parse(pri.body), 'info.version'), null, 2);
+const config = await resolveConfig(outFile);
+const output = await format(body, { ...config, parser: 'json' });
+
+await fs.writeFile(outFile, output);
